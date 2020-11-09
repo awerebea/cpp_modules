2175aa0001813b87d5942e29aee077ddf8a39c3a
awerebea <awerebea.21@gmail.com>
2020-11-09 13:49:17 +0300

    m01ex03 Add loop input number of zombies
    add check input string for number and 'atoi' analog (stringToNum)
    input loops while correct number not entered

--- module01/ex03/main.cpp
+++ module01/ex03/main.cpp
@@ -6,20 +6,62 @@
 /*   By: awerebea <awerebea@student.21-school.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 13:50:40 by awerebea          #+#    #+#             */
-/*   Updated: 2020/11/07 08:11:02 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/11/09 13:48:23 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ZombieHorde.hpp"
 #include <iostream>
+#include <cmath>
 
-int				main() {
-	int			N;
+int	isDecNum(std::string str)
+{
+	for (int i = 0; i < str.length(); i++) {
+		if (!((str[i] >= '0' && str[i] <= '9') \
+			|| (!i && (str[i] == '-' || str[i] == '+'))))
+			return (0);
+	}
+	return (1);
+}
+
+int	stringToNum(std::string str)
+{
+	long long int	result = 0;
+	int				sign = 1;
+	int				i = 0;
+
+	while (i < str.length() && (str[i] == '\n' || str[i] == '\t' \
+			|| str[i] == '\v' || str[i] == '\r' \
+			|| str[i] == '\f' || str[i] == ' '))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (i < str.length() && str[i] >= '0' && str[i]<= '9') {
+		if ((result * 10) < result) {
+			return (sign < 0 ? 0 : -1);
+		}
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return ((int)result * (int)sign);
+}
+
+int				main()
+{
+	int			N = -1;
+	std::string	input;
 
 	std::cout << "Input number of Zombies to create: ";
-	std::cin >> N;
-	if (!(N > 0))
-		std::cout << "Incorrect input. Qiut." << std::endl;
+	while (N < 0) {
+		std::getline(std::cin, input);
+		if (isDecNum(input) && (N = stringToNum(input)) >= 0) {
+			break ;
+		}
+		else
+			std::cout << "Incorrect input. Try again: ";
+	}
 	ZombieHorde		zombies = ZombieHorde(N);
 	zombies.announce();
 	return (0);
