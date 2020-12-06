beae2698ed8c01138c9e64dc141a4fc0a6793821
awerebea (pc-home) <awerebea.21@gmail.com>
2020-12-06 13:23:19 +0300

    m06ex00 Replace int array for flags with one char
    Optimize memory usage by replacing flag handling from an array of
    integers one char variable.

--- module06/ex00/Conv.cpp
+++ module06/ex00/Conv.cpp
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 15:19:08 by awerebea          #+#    #+#             */
-/*   Updated: 2020/12/05 19:12:29 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/12/06 13:19:59 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 Conv::Conv(const char * s) : _s(s)
 {
-	for (int i = 0; i < 4; i++)
-		_flags[i] = 0;
+	_flags = 0;
 	pseudoLiteral();
-	if (!_flags[0])
+	if (!_flags)
 	{
 		parser();
 	}
@@ -42,42 +41,40 @@ Conv &			Conv::operator=(Conv const & a)
 void			Conv::pseudoLiteral()
 {
 	if (_s == "+inf") {
-		for (int i = 0; i < 4; i++) _flags[i] = 2;
+		_flags = 2;
 		_d = std::numeric_limits<float>::infinity();
 		fromDouble();
 	}
 	else if (_s == "+inff") {
-		for (int i = 0; i < 4; i++) _flags[i] = 2;
+		_flags = 2;
 		_f = std::numeric_limits<float>::infinity();
 		fromFloat();
 	}
 	else if (_s == "-inf") {
-		for (int i = 0; i < 4; i++) _flags[i] = 3;
+		_flags = 4;
 		_d = -std::numeric_limits<float>::infinity();
 		fromDouble();
 	}
 	else if (_s == "-inff") {
-		for (int i = 0; i < 4; i++) _flags[i] = 3;
+		_flags = 4;
 		_f = -std::numeric_limits<float>::infinity();
 		fromFloat();
 	}
 	else if (_s == "nan") {
-		for (int i = 0; i < 4; i++) _flags[i] = 4;
+		_flags = 8;
 		_d = std::numeric_limits<float>::quiet_NaN();
 		fromDouble();
 	}
 	else if (_s == "nanf") {
-		for (int i = 0; i < 4; i++) _flags[i] = 4;
+		_flags = 8;
 		_f = std::numeric_limits<float>::quiet_NaN();
 		fromFloat();
 	}
 }
 
-int				checkStr(std::string const & s, int len)
+int				checkStr(std::string const & s, char & flags, int len)
 // return values: 0 - invalid string, 1 - int, 2 - float, 3 - double
 {
-	int			dotFlag = 0;
-
 	for (int i = 0; i < len; ++i)
 	{
 		if (!i && !(std::isdigit(s[0]) || s[0] == '-' || s[0] == '+'))
@@ -86,42 +83,33 @@ int				checkStr(std::string const & s, int len)
 			return (0);
 		else if (s[i] == '.')
 		{
-			if (dotFlag || i == len - 1)
+			if (flags & 32 || i == len - 1)
 				return (0);
-			dotFlag = 1;
+			flags += 32;
 		}
 		else if (i && !std::isdigit(s[i]) && !(i == len - 1 && s[i] == 'f' && \
-					std::isdigit(s[i - 1]) && dotFlag))
+					std::isdigit(s[i - 1]) && flags & 32))
 			return (0);
 	}
-	if (!dotFlag)
+	if (!(flags & 32))
 		return (1);
 	return (s[len - 1] == 'f' ? 2 : 3);
 }
 
-int				checkOverflow(long double num, int * _flags, int type)
+int				checkOverflow(long double num, char & _flags, int type)
 {
 	if (type == 1 && \
 		((num < std::numeric_limits<int>::min() || \
 		num > std::numeric_limits<int>::max())))
-		{
-			for (int i = 0; i < 4; i++) { _flags[i] = 1; }
-			return (1);
-		}
-	else if (type == 2 && \
+			return (_flags = 1);
+	else if (type == 2 && (num && \
 		((std::fabs(num) < std::fabs(std::numeric_limits<float>::min()) || \
-		std::fabs(num) > std::fabs(std::numeric_limits<float>::max()))))
-		{
-			for (int i = 0; i < 4; i++) { _flags[i] = 1; }
-			return (1);
-		}
-	else if (type == 3 && \
+		std::fabs(num) > std::fabs(std::numeric_limits<float>::max())))))
+			return (_flags = 1);
+	else if (type == 3 && (num && \
 		((std::fabs(num) < std::fabs(std::numeric_limits<double>::min()) || \
-		std::fabs(num) > std::fabs(std::numeric_limits<double>::max()))))
-		{
-			for (int i = 0; i < 4; i++) { _flags[i] = 1; }
-			return (1);
-		}
+		std::fabs(num) > std::fabs(std::numeric_limits<double>::max())))))
+			return (_flags = 1);
 	return (0);
 }
 
@@ -134,9 +122,9 @@ void			Conv::parser()
 		_c = _s[0];
 		fromChar();
 	}
-	else if (!(type = checkStr(_s, len)))
+	else if (!(type = checkStr(_s, _flags, len)))
 	{
-		for (int i = 0; i < 4; i++) _flags[i] = 1;
+		_flags = 1;
 		return ;
 	}
 	if (type == 1 && !checkOverflow(std::stold(_s), _flags, type))
@@ -170,10 +158,10 @@ void			Conv::fromInt()
 
 	if (_i >= 32 && _i <= 126)
 		_c = static_cast<char>(_i);
-	else if (!_flags[0] && _i >= 0 && _i <= 127)
-		_flags[0] = 5;
+	else if (!(_flags & 15) && _i >= 0 && _i <= 127)
+		_flags = 16;
 	else
-		_flags[0] = 1;
+		_flags += 64;
 }
 
 void			Conv::fromFloat()
@@ -183,28 +171,28 @@ void			Conv::fromFloat()
 
 	if (_i >= 32 && _i <= 126)
 		_c = static_cast<char>(_f);
-	else if (!_flags[0] && _i >= 0 && _i <= 127)
-		_flags[0] = 5;
+	else if (!(_flags & 15) && _i >= 0 && _i <= 127)
+		_flags = 16;
 	else
-		_flags[0] = 1;
+		_flags += 64;
 }
 
 void			Conv::fromDouble()
 {
 	_i = static_cast<int>(_d);
 	if (_d > std::numeric_limits<float>::max())
-		_flags[2] = 2;
+		_flags = 2;
 	_f = static_cast<float>(_d);
 
 	if (_i >= 32 && _i <= 126)
 		_c = static_cast<char>(_d);
-	else if (!_flags[0] && _i >= 0 && _i <= 127)
-		_flags[0] = 5;
+	else if (!(_flags & 15) && _i >= 0 && _i <= 127)
+		_flags = 16;
 	else
-		_flags[0] = 1;
+		_flags += 64;
 }
 
-int const *		Conv::getFlags() const { return (_flags); }
+char const &	Conv::getFlags() const { return (_flags); }
 
 char			Conv::getChar() const { return (_c); }
 
@@ -216,35 +204,35 @@ double			Conv::getDouble() const { return (_d); }
 
 std::ostream &	operator<<(std::ostream &out, Conv const & a)
 {
-	const int *		flags = a.getFlags();
+	const char &	flags = a.getFlags();
 
 	std::cout.precision(1);
 	out << std::setiosflags(std::ios::fixed);
 	out << "char: ";
-	if (flags[0])
-		out << (flags[0] < 5 ? "impossible" : "Non displayable");
+	if (flags & 95)
+		out << (flags & 16 ? "Non displayable" : "impossible");
 	else
 		out << "'" << a.getChar() << "'";
 	out << std::endl;
 	out << "int: ";
-	(flags[1] ? out << "impossible" : out << a.getInt());
+	(flags & 1 ? out << "impossible" : out << a.getInt());
 	out << std::endl;
 	out << "float: ";
-	if (flags[2] == 1)
+	if (flags & 1)
 		out << "impossible";
 	else
 	{
-		if (flags[2] == 2)
+		if (flags & 2)
 			out << "+";
 		out << a.getFloat() << "f";
 	}
 	out << std::endl;
 	out << "double: ";
-	if (flags[3] == 1)
+	if (flags & 1)
 		out << "impossible";
 	else
 	{
-		if (flags[3] == 2)
+		if (flags & 2)
 			out << "+";
 		out << a.getDouble();
 	}
--- module06/ex00/Conv.hpp
+++ module06/ex00/Conv.hpp
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 15:10:11 by awerebea          #+#    #+#             */
-/*   Updated: 2020/12/01 20:28:10 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/12/06 12:45:00 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,18 @@
 class						Conv
 {
 	std::string				_s;
-	int						_flags[4];	// [0] - char,  [1] - int,
-										// [2] - float, [3] - double
-							// 0 - normal, 1 - incorrect/overflow (impossible),
-							// 2 - +inf, 3 - -inf, 4 - nan, 5 - non displ.
 	char					_c;
 	int						_i;
 	float					_f;
 	double					_d;
+	char					_flags; // 0b00000000 (0) - normal,
+									// 0b00000001 (1) - incorrect/overflow,
+									// 0b00000010 (2) - +inf,
+									// 0b00000100 (4) - -inf,
+									// 0b00001000 (8) - nan,
+									// 0b00010000 (16) - non displayable,
+									// 0b00100000 (32) - dot in number,
+									// 0b01000000 (64) - overflow casted char
 
 	void					pseudoLiteral();
 	void					parser();
@@ -45,7 +49,7 @@ public:
 
 	Conv &					operator=(Conv const & a);
 
-	int const *				getFlags() const;
+	char const &			getFlags() const;
 
 	char					getChar() const;
 	int						getInt() const;
